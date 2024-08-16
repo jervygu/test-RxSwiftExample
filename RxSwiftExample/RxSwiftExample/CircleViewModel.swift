45c73025ca9dab22de3f2854a140eb482a948c61
Jervy Umandap <jegumandap@unionbankph.com>
2024-08-16 16:54:27 +0800

    ### Observable and the Bind

---
+++ RxSwiftExample/RxSwiftExample/CircleViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  CircleViewModel.swift
+//  RxSwiftExample
+//
+//  Created by Jervy Umandap on 8/16/24.
+//
+
+import Foundation
+import ChameleonFramework
+import RxSwift
+import RxCocoa
+
+class CircleViewModel {    
+    
+    var centerVariable = BehaviorRelay<CGPoint?>(value: .zero)
+    var backgroundColorObservable: Observable<UIColor>!
+    
+    init() {
+        setup()
+    }
+
+    func setup() {
+        // When we get new center, emit new UIColor
+        backgroundColorObservable = centerVariable.asObservable()
+            .map { center in
+                guard let center else { return UIColor.flatten(.black)() }
+                
+                let red: CGFloat = ((center.x + center.y).truncatingRemainder(dividingBy: 255.0)) / 255.0 // // We just manipulate red, but you can do w/e
+                let green: CGFloat = 0.0
+                let blue: CGFloat = 0.0
+                
+                return UIColor.flatten(UIColor(red: red, green: green, blue: blue, alpha: 1.0))()
+            }
+    }
+    
+}
--- RxSwiftExample/RxSwiftExample/HomeViewController.swift
+++ RxSwiftExample/RxSwiftExample/HomeViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import ChameleonFramework
+import RxSwift
+import RxCocoa
 
 class HomeViewController: UIViewController {
+    
+    var circleView: UIView!
+    let circleViewModel = CircleViewModel()
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Home"
-        view.backgroundColor = .systemGreen
+        view.backgroundColor = .systemBackground
+        setup()
+    }
+    
+    func setup() {
+        // Add circle view
+        circleView = UIView(frame: CGRect(origin: view.center, size: CGSize(width: 100.0, height: 100.0)))
+        circleView.layer.cornerRadius = circleView.frame.width / 2.0
+        circleView.center = view.center
+        circleView.backgroundColor = .systemGreen
+        view.addSubview(circleView)
+        
+        circleView
+            .rx.observe(CGPoint.self, "center")
+            .bind(to: circleViewModel.centerVariable)
+            .disposed(by: disposeBag)
+        
+        circleViewModel.backgroundColorObservable
+            .subscribe { [weak self] backgroundColor in
+                UIView.animate(withDuration: 0.1) {
+                    self?.circleView.backgroundColor = backgroundColor
+                    
+                    let viewBackgroundColor  = UIColor(complementaryFlatColorOf: backgroundColor)
+                    
+                    if viewBackgroundColor != backgroundColor {
+                        self?.view.backgroundColor = viewBackgroundColor
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        
+        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(circleMoved))
+        circleView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func circleMoved(_ recognizer: UIPanGestureRecognizer) {
+        let location = recognizer.location(in: view)
+        UIView.animate(withDuration: 0.1) {
+            self.circleView.center = location
+        }
     }
     
 }
