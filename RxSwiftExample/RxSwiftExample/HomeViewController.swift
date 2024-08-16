1885d0952d0d2e1337c5d6779e1171093277e56b
Jervy Umandap <jegumandap@unionbankph.com>
2024-08-16 11:26:01 +0800

    ### TableViewController setup

---
+++ RxSwiftExample/RxSwiftExample/HomeViewController.swift
@@ -0,0 +1,49 @@
+//
+//  HomeViewController.swift
+//  RxSwiftExample
+//
+//  Created by Jervy Umandap on 8/16/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var shownCities = [String]() // Data source for UITableView
+    let allCities = ["New York", "London", "Oslo", "Warsaw", "Berlin", "Praga"] // Our mocked API data source
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        title = "Home"
+    }
+
+    // MARK: - Table view data source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allCities.count
+    }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let item = allCities[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = item
+        content.secondaryText = "SecondaryText"
+        content.image = UIImage(systemName: "person.circle")
+        cell.contentConfiguration = content
+        return cell
+    }
+}
---
+++ RxSwiftExample/RxSwiftExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftExample
+//
+//  Created by Jervy Umandap on 8/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
