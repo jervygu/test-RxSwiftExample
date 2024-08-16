1883b66449db4b877978e77fd5bb5e5bf62d0f85
Jervy Umandap <jegumandap@unionbankph.com>
2024-08-16 16:06:27 +0800

    Revert "### Initial setup"
    This reverts commit 420d5acb21675fc933b57d7c430f655abd61ab8f.

--- RxSwiftExample/RxSwiftExample/HomeViewController.swift
+++ RxSwiftExample/RxSwiftExample/HomeViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var shownCities = [String]() // Data source for UITableView
+    let allCities = ["New York", "London", "Oslo", "Warsaw", "Berlin", "Praga"] // Our mocked API data source
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         title = "Home"
-        view.backgroundColor = .systemGreen
     }
+
+    // MARK: - Table view data source
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allCities.count
+    }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let item = allCities[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = item
+        content.secondaryText = "SecondaryText"
+        content.image = UIImage(systemName: "person.circle")
+        cell.contentConfiguration = content
+        return cell
+    }
 }
